667411785f2023ae6c9e553e59595e6353330d7f
jaeheonYoo325 <yoojh0325@gmail.com>
2021-09-09 22:20:58 +0900

    2021.09.09 : 준영속 상태.

---
+++ src/main/java/com/jpabasic/JpaEx5.java
@@ -0,0 +1,45 @@
+package com.jpabasic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaEx5 {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+		
+		EntityManager em = emf.createEntityManager();
+		
+		EntityTransaction tx = em.getTransaction();
+		tx.begin();
+		
+		try {
+			
+			//영속
+			Member member = em.find(Member.class, 150L);
+			member.setName("AAAAA");
+
+			//준영속 상태
+			//em.detach(member);
+			
+			//영속성 컨텍스트 완전히 초기화
+			em.clear();
+			
+			Member member2 = em.find(Member.class, 150L);
+			
+			System.out.println("=================");
+			
+			tx.commit();
+		} catch (Exception e) {
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+		
+		emf.close();
+
+	}
+
+}
